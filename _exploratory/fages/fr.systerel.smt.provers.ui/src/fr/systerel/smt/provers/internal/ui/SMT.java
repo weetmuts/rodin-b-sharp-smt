2acd2dd3851ecb2af1ce86f019cf8a43b03e71a8
Yoann Fages-Tafanelli <fages@users.sourceforge.net>
2010-09-14 15:30:30 +0000

    YFT: now SMT is an implementation of ITacticProvider
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk@9739 1434b563-b632-4741-aa49-43a3a8374d2e

--- _exploratory/fages/fr.systerel.smt.provers.ui/src/fr/systerel/smt/provers/internal/ui/SMT.java
+++ _exploratory/fages/fr.systerel.smt.provers.ui/src/fr/systerel/smt/provers/internal/ui/SMT.java
@@ -11,31 +11,38 @@
 
 package fr.systerel.smt.provers.internal.ui;
 
-import java.util.Collections;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
 import java.util.List;
 
-import org.eventb.core.ast.IPosition;
 import org.eventb.core.ast.Predicate;
 import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.core.seqprover.ITactic;
-import org.eventb.ui.prover.DefaultTacticProvider;
+import org.eventb.ui.prover.DefaultTacticProvider.DefaultPredicateApplication;
+import org.eventb.ui.prover.ITacticApplication;
+import org.eventb.ui.prover.ITacticProvider;
 
 import fr.systerel.smt.provers.core.SmtProversCore;
 
-public class SMT extends DefaultTacticProvider {
+public class SMT implements ITacticProvider {
 
-	@Override
-	public ITactic getTactic(IProofTreeNode node, Predicate hyp,
-			IPosition position, String[] inputs, String globalInput) {
-		return SmtProversCore.ExternalSmtTac(true);
-	}
+	public static class SMTApplication extends DefaultPredicateApplication {
 
-	@Override
-	public List<IPosition> getApplicablePositions(IProofTreeNode node,
-			Predicate hyp, String input) {
-		if (node != null && node.isOpen())
-			return Collections.<IPosition>emptyList();
-		return null;
+		@Override
+		public ITactic getTactic(String[] inputs, String globalInput) {
+			return SmtProversCore.ExternalSmtTac(true);
+		}
+		
+	} 
+	
+	public List<ITacticApplication> getPossibleApplications(
+			IProofTreeNode node, Predicate hyp, String globalInput) {
+		if (node != null && node.isOpen()) {
+			final ITacticApplication appli = new SMTApplication();
+			return singletonList(appli);
+		}
+		return emptyList();
 	}
 
 }
