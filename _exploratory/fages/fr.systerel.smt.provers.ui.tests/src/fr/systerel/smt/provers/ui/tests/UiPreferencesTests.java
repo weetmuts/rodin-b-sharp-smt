8f130e37706a85cebfa12c65bbe87709a6623014
Vitor Alcântara de Almeida <vitoralmeida@users.sourceforge.net>
2011-04-13 14:19:06 +0000

    Adding the smt test ui plugin elements
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk@11234 1434b563-b632-4741-aa49-43a3a8374d2e

---
+++ _exploratory/fages/fr.systerel.smt.provers.ui.tests/src/fr/systerel/smt/provers/ui/tests/UiPreferencesTests.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package fr.systerel.smt.provers.ui.tests;
+
+import static br.ufrn.smt.solver.preferences.SMTPreferencesStore.CreatePreferences;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ufrn.smt.solver.preferences.SMTPreferences;
+import br.ufrn.smt.solver.preferences.SolverDetail;
+
+import fr.systerel.smt.provers.core.SmtProversCore;
+import fr.systerel.smt.provers.ui.SmtProversUIPlugin;
+
+/**
+ * @author vitor
+ * 
+ */
+public class UiPreferencesTests {
+
+	public static final String PREPROPATH = "prepropath";
+
+	public static final String USING_PREPRO = "usingprepro";
+
+	public static final String SOLVER_INDEX = "solverindex";
+
+	public static final String SOLVER_PREFERENCES = "solverpreferences";
+
+	public static final String RODIN_SEQUENT = "rodin_sequent";
+
+	public static final String REASONER_ID = SmtProversCore.PLUGIN_ID
+			+ ".externalSMT";
+
+	public static final String PREFERENCES_ID = "fr.systerel.smt.provers.ui";
+
+	/**
+	 * In linux: '/home/username/bin/'
+	 */
+	protected static final String BIN_PATH = System.getProperty("user.home")
+			+ System.getProperty("file.separator") + "bin"
+			+ System.getProperty("file.separator");
+
+	protected static void setSolverPreferences(final String solverBinaryName,
+			final String solverArgs, final boolean isSMTV1_2Compatible,
+			final boolean isSMTV2_0Compatible) {
+		final String OS = System.getProperty("os.name");
+		final IPreferenceStore store = SmtProversUIPlugin
+				.getDefaultPreferenceStore();
+		final String solverPath;
+
+		if (OS.startsWith("Windows")) {
+			solverPath = BIN_PATH + solverBinaryName + ".exe";
+		} else {
+			solverPath = BIN_PATH + solverBinaryName;
+		}
+
+		System.out.println(solverPath);
+
+		final List<SolverDetail> solvers = new ArrayList<SolverDetail>();
+		solvers.add(new SolverDetail(solverBinaryName, solverPath, solverArgs,
+				isSMTV1_2Compatible, isSMTV2_0Compatible));
+		final String preferences = CreatePreferences(solvers);
+		store.setValue("solverpreferences", preferences);
+		store.setValue("solverindex", 0);
+		store.setValue("usingprepro", true);
+		store.setValue("prepropath", BIN_PATH + "verit");
+	}
+
+	protected static void setPreferencesForVeriTTest() {
+		setSolverPreferences("verit", "", true, false);
+	}
+
+	protected void setPreferencesForCvc3Test() {
+		setSolverPreferences("cvc3", "-lang smt", true, false);
+
+	}
+
+	protected void setPreferencesForZ3Test() {
+		String solver = "z3";
+		if (System.getProperty("os.name").startsWith("Windows")) {
+			solver = "bin" + System.getProperty("file.separator") + solver
+					+ System.getProperty("file.separator") + "bin"
+					+ System.getProperty("file.separator") + "z3";
+		}
+
+		setSolverPreferences(solver, "", true, false);
+	}
+
+	protected void setPreferencesForAltErgoTest() {
+		setSolverPreferences("alt-ergo", "", true, false);
+	}
+
+	@Test
+	public void testRecoverPreferences1() {
+		setPreferencesForAltErgoTest();
+
+		final IPreferencesService preferencesService = Platform
+				.getPreferencesService();
+
+		/**
+		 * Get back preferences from UI
+		 */
+		String solverPreferencesString = preferencesService.getString(
+				PREFERENCES_ID, SOLVER_PREFERENCES, null, null);
+		int solverIndex = preferencesService.getInt(PREFERENCES_ID,
+				SOLVER_INDEX, -1, null);
+		boolean usingPrePro = preferencesService.getBoolean(PREFERENCES_ID,
+				USING_PREPRO, false, null);
+		String preProPath = preferencesService.getString(PREFERENCES_ID,
+				PREPROPATH, null, null);
+		SMTPreferences smtPreferences = new SMTPreferences(
+				solverPreferencesString, solverIndex, usingPrePro, preProPath);
+
+		String expectedId = "alt-ergo";
+		String expectedSolverPath = BIN_PATH + expectedId;
+		if (System.getProperty("os.name").startsWith("Windows")) {
+			expectedSolverPath += ".exe";
+		}
+		String args = "";
+		boolean smtV1_2 = true;
+		boolean smtV2_0 = false;
+		boolean expectedPrePro = true;
+		String expectedPreproPath = BIN_PATH + "verit";
+
+		SolverDetail expectedSolverDetail = new SolverDetail(expectedId,
+				expectedSolverPath, args, smtV1_2, smtV2_0);
+		SMTPreferences expectedSMTPreferences = new SMTPreferences(
+				expectedSolverDetail, expectedPrePro, expectedPreproPath);
+
+		SMTPreferences[] p = { smtPreferences };
+		SMTPreferences[] expP = { expectedSMTPreferences };
+
+		Assert.assertArrayEquals(expP, p);
+
+	}
+}
