51a9575c99412341914ea1634f465bf7fd78a5c9
Yoann Guyot <yguyot@users.sourceforge.net>
2011-06-09 14:20:34 +0000

    Added the logic determination tests to the regression test suite.
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk@11929 1434b563-b632-4741-aa49-43a3a8374d2e

--- _exploratory/fages/fr.systerel.smt.provers.core.tests/src/fr/systerel/smt/provers/core/tests/QuickRegressionTestSuite.java
+++ _exploratory/fages/fr.systerel.smt.provers.core.tests/src/fr/systerel/smt/provers/core/tests/QuickRegressionTestSuite.java
@@ -6,13 +6,14 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import fr.systerel.smt.provers.core.tests.acceptance.RunProverTestWithPP;
 import fr.systerel.smt.provers.core.tests.acceptance.RunProverTestWithVeriT;
+import fr.systerel.smt.provers.core.tests.unit.LogicTestsWithPP;
 import fr.systerel.smt.provers.core.tests.unit.TranslationTestsWithPP;
 import fr.systerel.smt.provers.core.tests.unit.TranslationTestsWithVeriT;
 
 @RunWith(Suite.class)
 @SuiteClasses(value = { TranslationTestsWithPP.class,
-		TranslationTestsWithVeriT.class, RunProverTestWithPP.class,
-		RunProverTestWithVeriT.class })
+		TranslationTestsWithVeriT.class, LogicTestsWithPP.class,
+		RunProverTestWithPP.class, RunProverTestWithVeriT.class })
 public class QuickRegressionTestSuite {
 	// Just for tests
 }
