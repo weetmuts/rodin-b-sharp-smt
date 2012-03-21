f3c662070d6b6d508734173e6ff2dac029c77b70
Yoann Guyot <yguyot@users.sourceforge.net>
2012-03-21 16:56:13 +0000

    Moved the bundled solver loading exception class into a generic one (to be used for bundled solvers and solvers configurations parts).
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk@14396 1434b563-b632-4741-aa49-43a3a8374d2e

---
+++ _exploratory/fages/org.eventb.smt.core/src/org/eventb/smt/core/preferences/ExtensionLoadingException.java
@@ -0,0 +1,55 @@
+package org.eventb.smt.core.preferences;
+
+public class ExtensionLoadingException extends IllegalArgumentException {
+	public static final ExtensionLoadingException makeDotInIDException(
+			final String id) {
+		return new ExtensionLoadingException("Invalid id: " + id
+				+ " (must not contain a dot).");
+	}
+
+	public static final ExtensionLoadingException makeInvalidJavaIDException(
+			final String id) {
+		return new ExtensionLoadingException("Invalid id: " + id
+				+ " (must be a valid Java identifier).");
+	}
+
+	public static final ExtensionLoadingException makeNullIDException() {
+		return new ExtensionLoadingException("Invalid id: null pointer.");
+	}
+
+	public static final ExtensionLoadingException makeNullBinaryNameException() {
+		return new ExtensionLoadingException(
+				"Invalid binary name: null pointer.");
+	}
+
+	public static final ExtensionLoadingException makeNoSuchBundleException(
+			final String bundleName) {
+		return new ExtensionLoadingException("Invalid bundle name: "
+				+ bundleName + " (no such bundle was found installed).");
+	}
+
+	public static final ExtensionLoadingException makeNullPathException() {
+		return new ExtensionLoadingException("Invalid path: null pointer.");
+	}
+
+	public static final ExtensionLoadingException makeNoExtensionException(
+			final String id) {
+		return new ExtensionLoadingException("Invalid extension point id: "
+				+ id + " (no such extension point was found).");
+	}
+
+	public static final ExtensionLoadingException makeIllegalExtensionException(
+			final String id) {
+		return new ExtensionLoadingException("Duplicated extension " + id
+				+ " ignored.");
+	}
+
+	private ExtensionLoadingException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Generated serial version ID.
+	 */
+	private static final long serialVersionUID = -2787953160141168010L;
+}
