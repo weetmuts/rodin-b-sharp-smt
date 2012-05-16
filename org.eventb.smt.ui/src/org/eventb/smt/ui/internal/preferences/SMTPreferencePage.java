a874dbaf845bf3bcf7c2be76c15802646e88df1b
Laurent Voisin <laurent.voisin@systerel.fr>
2012-05-16 17:32:58 +0200

    Move Translation preferences to the main SMT preference page
    The main SMT preference page was empty, which looks odd.  I've therefore
    moved the contents of the Translation preference page to the main
    preference page and suppressed the Translation page.
    
    Also added that the translation directory must not be an empty string.
    We still allow an empty string for the veriT binary, as it is not
    required when using PP translation.

--- org.eventb.smt.ui/src/org/eventb/smt/ui/internal/preferences/SMTPreferencePage.java
+++ org.eventb.smt.ui/src/org/eventb/smt/ui/internal/preferences/SMTPreferencePage.java
@@ -9,9 +9,22 @@
  *******************************************************************************/
 package org.eventb.smt.ui.internal.preferences;
 
+import static org.eclipse.jface.resource.JFaceResources.getString;
+import static org.eventb.smt.core.preferences.PreferenceManager.TRANSLATION_PATH_ID;
+import static org.eventb.smt.core.preferences.PreferenceManager.VERIT_PATH_ID;
+
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FileFieldEditor;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.eventb.smt.core.SMTCore;
 
 /**
  * @author yguyot
@@ -19,13 +32,61 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 public class SMTPreferencePage extends FieldEditorPreferencePage implements
 		IWorkbenchPreferencePage {
 
-	@Override
-	public void createFieldEditors() {
-		// Do nothing at the moment.
-	}
+	private static final String SMT_TRANSLATION_SETTINGS_LABEL = "SMT translation settings:";
+	private static final String VERIT_PATH_LABEL = "veriT path";
+	private static final String TRANSLATION_PATH_LABEL = "Temporary translation files path";
 
 	@Override
-	public void init(IWorkbench workbench) {
-		setDescription("Settings for the SMT-Solvers Plug-in");
+	public void init(final IWorkbench workbench) {
+		setDescription(SMT_TRANSLATION_SETTINGS_LABEL);
 	}
+
+	/*
+	 * The preferences are actually stored in the SMT core plug-in.
+	 */
+	@Override
+	protected IPreferenceStore doGetPreferenceStore() {
+		return new ScopedPreferenceStore(InstanceScope.INSTANCE,
+				SMTCore.PLUGIN_ID);
+	}
+
+	/*
+	 * It is on purpose that getFieldEditorParent() is called for each field, it
+	 * is mandated by the FieldEditor API. Do not share this code.
+	 */
+	@Override
+	protected void createFieldEditors() {
+		final StringFieldEditor translationDirBrowser = new TranslationDirectoryEditor(
+				TRANSLATION_PATH_ID, TRANSLATION_PATH_LABEL,
+				getFieldEditorParent());
+		translationDirBrowser.setEmptyStringAllowed(false);
+		addField(translationDirBrowser);
+
+		final FieldEditor veriTBinaryBrowser = new FileFieldEditor(
+				VERIT_PATH_ID, VERIT_PATH_LABEL, true, getFieldEditorParent());
+		addField(veriTBinaryBrowser);
+	}
+
+	/*
+	 * We want a directory field editor that validates the user input on every
+	 * keystroke, because, otherwise, the "Apply" and "OK" button are not
+	 * disabled when the field is edited directly by the user and is incorrect.
+	 *
+	 * However, the DirectoryFieldEditor does not allow this, unless we call the
+	 * default constructor and provide all information afterwards. We therefore
+	 * sub-class DirectoryFieldEditor, copy its constructor, just changing one
+	 * line to set the validation strategy we want. This is a pity.
+	 */
+	private static class TranslationDirectoryEditor extends
+			DirectoryFieldEditor {
+		public TranslationDirectoryEditor(String name, String label,
+				Composite parent) {
+			init(name, label);
+			setErrorMessage(getString("DirectoryFieldEditor.errorMessage"));
+			setChangeButtonText(getString("openBrowse"));
+			setValidateStrategy(VALIDATE_ON_KEY_STROKE);
+			createControl(parent);
+		}
+	}
+
 }
