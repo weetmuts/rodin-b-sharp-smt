6591331298aae89868c932b7e1db0d188271d569
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-03-02 19:37:34 +0100

    Refactor tests for tactics
    Move code to the common super class so that it can be reused in future
    tests.

--- org.eventb.smt.core.tests/src/org/eventb/smt/core/tests/tactics/SMTAutoTacticTests.java
+++ org.eventb.smt.core.tests/src/org/eventb/smt/core/tests/tactics/SMTAutoTacticTests.java
@@ -9,12 +9,7 @@
  *******************************************************************************/
 package org.eventb.smt.core.tests.tactics;
 
-import static org.eventb.smt.core.SMTCore.newConfigDescriptor;
-import static org.junit.Assert.assertTrue;
-
 import org.eventb.smt.core.SMTCore;
-import org.eventb.smt.core.prefs.IConfigDescriptor;
-import org.junit.AfterClass;
 import org.junit.Test;
 
 /**
@@ -23,17 +18,6 @@ import org.junit.Test;
  * @author Laurent Voisin
  */
 public class SMTAutoTacticTests extends TacticTests {
-
-	private static final IConfigDescriptor[] originalConfigs = SMTCore
-			.getConfigurations();
-
-	/*
-	 * Puts back the original list of configurations after these tests.
-	 */
-	@AfterClass
-	public static void resetAllConfigurations() {
-		SMTCore.setConfigurations(originalConfigs);
-	}
 
 	/**
 	 * Ensures that the SMT auto-tactic can discharge a simple sequent.
@@ -51,29 +35,6 @@ public class SMTAutoTacticTests extends TacticTests {
 	public void smtAutoCanFail() {
 		enableAllConfigurations(false);
 		assertFails(SMTCore.smtAutoTactic);
-	}
-
-	/*
-	 * Change the enablement of all known SMT configurations to the given value.
-	 */
-	public static void enableAllConfigurations(boolean enabled) {
-		final int length = originalConfigs.length;
-		assertTrue("No known SMT configuration !", length != 0);
-		final IConfigDescriptor[] newConfigs = new IConfigDescriptor[length];
-		for (int i = 0; i < length; i++) {
-			newConfigs[i] = makeEnabled(originalConfigs[i], enabled);
-		}
-		SMTCore.setConfigurations(newConfigs);
-	}
-
-	/*
-	 * Returns an enabled copy of the given descriptor.
-	 */
-	private static IConfigDescriptor makeEnabled(IConfigDescriptor desc,
-			boolean enabled) {
-		return newConfigDescriptor(desc.getName(), desc.getSolverName(),
-				desc.getArgs(), desc.getTranslationApproach(),
-				desc.getSmtlibVersion(), enabled);
 	}
 
 }
--- org.eventb.smt.core.tests/src/org/eventb/smt/core/tests/tactics/TacticTests.java
+++ org.eventb.smt.core.tests/src/org/eventb/smt/core/tests/tactics/TacticTests.java
@@ -10,7 +10,8 @@
 package org.eventb.smt.core.tests.tactics;
 
 import static org.eventb.core.seqprover.ProverFactory.makeProofTree;
-import static org.eventb.core.seqprover.tests.TestLib.genSeq;
+import static org.eventb.core.seqprover.tests.TestLib.genFullSeq;
+import static org.eventb.smt.core.SMTCore.newConfigDescriptor;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -21,6 +22,9 @@ import org.eventb.core.seqprover.IProofTree;
 import org.eventb.core.seqprover.IProofTreeNode;
 import org.eventb.core.seqprover.IProverSequent;
 import org.eventb.core.seqprover.ITactic;
+import org.eventb.smt.core.SMTCore;
+import org.eventb.smt.core.prefs.IConfigDescriptor;
+import org.junit.AfterClass;
 
 /**
  * Utility methods for testing tactics.
@@ -28,6 +32,42 @@ import org.eventb.core.seqprover.ITactic;
  * @author Laurent Voisin
  */
 public abstract class TacticTests {
+
+	private static final String SIMPLE_SEQUENT = ";H; ;S; |- 1+1 = 2";
+
+	private static final IConfigDescriptor[] originalConfigs = SMTCore
+			.getConfigurations();
+
+	/*
+	 * Puts back the original list of configurations after these tests.
+	 */
+	@AfterClass
+	public static void resetAllConfigurations() {
+		SMTCore.setConfigurations(originalConfigs);
+	}
+
+	/*
+	 * Change the enablement of all known SMT configurations to the given value.
+	 */
+	public static void enableAllConfigurations(boolean enabled) {
+		final int length = originalConfigs.length;
+		assertTrue("No known SMT configuration !", length != 0);
+		final IConfigDescriptor[] newConfigs = new IConfigDescriptor[length];
+		for (int i = 0; i < length; i++) {
+			newConfigs[i] = makeEnabled(originalConfigs[i], enabled);
+		}
+		SMTCore.setConfigurations(newConfigs);
+	}
+
+	/*
+	 * Returns an enabled copy of the given descriptor.
+	 */
+	private static IConfigDescriptor makeEnabled(IConfigDescriptor desc,
+			boolean enabled) {
+		return newConfigDescriptor(desc.getName(), desc.getSolverName(),
+				desc.getArgs(), desc.getTranslationApproach(),
+				desc.getSmtlibVersion(), enabled);
+	}
 
 	/**
 	 * Checks that the given tactic can discharge a simple sequent. The used
@@ -37,8 +77,22 @@ public abstract class TacticTests {
 	 *            the descriptor of the tactic to run
 	 */
 	protected static void assertDischarges(ITacticDescriptor desc) {
+		assertDischarges(desc, SIMPLE_SEQUENT);
+	}
+
+	/**
+	 * Checks that the given tactic can discharge the given sequent.
+	 * 
+	 * @param desc
+	 *            the descriptor of the tactic to run
+	 * @param sequentImage
+	 *            the sequent to discharge as a string
+	 * 
+	 */
+	protected static void assertDischarges(ITacticDescriptor desc,
+			String sequentImage) {
 		final ITactic tactic = desc.getTacticInstance();
-		final IProofTreeNode node = makeSimpleProofTreeNode();
+		final IProofTreeNode node = makeProofTreeNode(sequentImage);
 		final Object result = tactic.apply(node, null);
 		assertNull("The tactic failed", result);
 		assertTrue(node.isClosed());
@@ -64,7 +118,11 @@ public abstract class TacticTests {
 	 * discharged by any SMT solver.
 	 */
 	private static IProofTreeNode makeSimpleProofTreeNode() {
-		final IProverSequent sequent = genSeq("|- 1+1 = 2");
+		return makeProofTreeNode(SIMPLE_SEQUENT);
+	}
+
+	private static IProofTreeNode makeProofTreeNode(final String sequentImage) {
+		final IProverSequent sequent = genFullSeq(sequentImage);
 		final IProofTree proofTree = makeProofTree(sequent, null);
 		final IProofTreeNode node = proofTree.getRoot();
 		assertFalse(node.isClosed());
