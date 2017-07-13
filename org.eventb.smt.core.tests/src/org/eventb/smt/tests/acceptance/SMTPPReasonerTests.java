bf34b9c48423ade951c5e1757d778b62b93e0313
Nicolas Beauger <nicolas.beauger@systerel.fr>
2017-07-13 16:47:49 +0200

    Add missing acceptance tests for CVC4 and Z3

--- org.eventb.smt.core.tests/src/org/eventb/smt/tests/acceptance/SMTPPReasonerTests.java
+++ org.eventb.smt.core.tests/src/org/eventb/smt/tests/acceptance/SMTPPReasonerTests.java
@@ -26,16 +26,26 @@ public class SMTPPReasonerTests extends AbstractReasonerTests {
 
 	public static final String BUNDLED_VERIT_PP_SMT2_ID = "veriT";
 	public static final String BUNDLED_CVC3_PP_SMT2_ID = "CVC3";
+	public static final String BUNDLED_CVC4_PP_SMT2_ID = "CVC4";
+	public static final String BUNDLED_Z3_PP_SMT2_ID = "Z3";
 
 	private static final boolean DEFAULT_RESTRICTED_VALUE = true;
 	private static final long DEFAULT_TIMEOUT_DELAY = 1000;
+
+	private static final SMTInput VERIT_INPUT = new SMTInput(
+			BUNDLED_VERIT_PP_SMT2_ID, DEFAULT_RESTRICTED_VALUE,
+			DEFAULT_TIMEOUT_DELAY);
 
 	private static final SMTInput CVC3_INPUT = new SMTInput(
 			BUNDLED_CVC3_PP_SMT2_ID, DEFAULT_RESTRICTED_VALUE,
 			DEFAULT_TIMEOUT_DELAY);
 
-	private static final SMTInput VERIT_INPUT = new SMTInput(
-			BUNDLED_VERIT_PP_SMT2_ID, DEFAULT_RESTRICTED_VALUE,
+	private static final SMTInput CVC4_INPUT = new SMTInput(
+			BUNDLED_CVC4_PP_SMT2_ID, DEFAULT_RESTRICTED_VALUE,
+			DEFAULT_TIMEOUT_DELAY);
+
+	private static final SMTInput Z3_INPUT = new SMTInput(
+			BUNDLED_Z3_PP_SMT2_ID, DEFAULT_RESTRICTED_VALUE,
 			DEFAULT_TIMEOUT_DELAY);
 
 	@Override
@@ -48,11 +58,15 @@ public class SMTPPReasonerTests extends AbstractReasonerTests {
 		assertReasonerSuccess(" x < y ;; y < z |- x < z ", VERIT_INPUT);
 		assertReasonerSuccess(" x < y ;; z = x + y ;; y < z |- x < z ",	VERIT_INPUT);
 		assertReasonerSuccess(" x < y ;; y < z |- x < z ", CVC3_INPUT);
+		assertReasonerSuccess(" x < y ;; y < z |- x < z ", CVC4_INPUT);
+		assertReasonerSuccess(" x < y ;; y < z |- x < z ", Z3_INPUT);
 	}
 
 	@Test
 	public void testFailure() throws Exception {
 		assertReasonerFailure(" x = 1 |- x = 2", VERIT_INPUT, "Failed");
 		assertReasonerFailure(" x = 1 |- x = 2", CVC3_INPUT, "Failed");
+		assertReasonerFailure(" x = 1 |- x = 2", CVC4_INPUT, "Failed");
+		assertReasonerFailure(" x = 1 |- x = 2", Z3_INPUT, "Failed");
 	}
 }
