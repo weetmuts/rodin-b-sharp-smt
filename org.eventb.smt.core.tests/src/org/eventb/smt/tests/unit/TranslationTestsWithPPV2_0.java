2ff74138305e3c72cb98f75b04ddd06c04ced837
Nicolas Beauger <nicolas.beauger@systerel.fr>
2017-07-13 17:13:13 +0200

    Fix testReservedWordsFuns
    This test checks the translation of a type environment.
    The translation iterates over the type environment,
    creating fresh variable names. The order of iteration depends
    on a (non linked) hash set, so the fresh names depend on
    the java implementation.
    
    Set variables names as produced by Java 8 hash sets.

--- org.eventb.smt.core.tests/src/org/eventb/smt/tests/unit/TranslationTestsWithPPV2_0.java
+++ org.eventb.smt.core.tests/src/org/eventb/smt/tests/unit/TranslationTestsWithPPV2_0.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2014 Systerel and others.
+ * Copyright (c) 2011, 2017 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -537,9 +537,9 @@ public class TranslationTestsWithPPV2_0 extends AbstractTests {
 
 		final Set<String> expectedFuns = new HashSet<String>(Arrays.asList( //
 				"BOOLS () PB", //
-				"nf1 () NS", //
+				"nf0 () NS", //
 				"INTS () PZ", //
-				"nf0 () PA", //
+				"nf1 () PA", //
 				"nf () NS"));
 
 		testTypeEnvironmentFuns(defaultLogic, te, expectedFuns,
