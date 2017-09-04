5e5abaec5c71d6dc99f2dfe08fa596b9e3d38588
Nicolas Beauger <nicolas.beauger@systerel.fr>
2017-09-04 18:37:11 +0200

    Core tests: rename veriT axioms test class
    Match the names of other child classes of the super class.

--- org.eventb.smt.core.tests/src/org/eventb/smt/tests/acceptance/verit/AxiomsTestWithVeriTPPV2_0.java
+++ org.eventb.smt.core.tests/src/org/eventb/smt/tests/acceptance/verit/AxiomsTestWithVeriTPPV2_0.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2013 Systerel and others.
+ * Copyright (c) 2011, 2017 Systerel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,9 +14,9 @@ import static org.eventb.smt.tests.ConfigProvider.BUNDLED_VERIT;
 
 import org.eventb.smt.tests.acceptance.AxiomsTestWithPPV2_0;
 
-public class AxiomsTestWithPP extends AxiomsTestWithPPV2_0 {
+public class AxiomsTestWithVeriTPPV2_0 extends AxiomsTestWithPPV2_0 {
 
-	public AxiomsTestWithPP() {
+	public AxiomsTestWithVeriTPPV2_0() {
 		super(BUNDLED_VERIT);
 	}
 
